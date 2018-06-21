4a8722d2f8c6dc46296592684c08bb58ded93241
Felix Xiong <Felixiong@Felixs-iMac.local>
2018-06-22 06:35:48 +1200

    updates 22.06.18

--- temp.r
+++ temp.r
@@ -68,3 +68,62 @@ h_gap$country %>% fct_relevel("Romania") %>% levels()
 # merge levels from different data frames
 ?fct_c
 
+
+### chapter 2
+library(stringr)
+
+# detect a literal string
+str_detect(fruit, pattern = 'fruit')
+fruit[str_detect(fruit, pattern = 'fruit')]
+# keep only the matching elements
+(my_fruit <- str_subset(fruit, pattern = 'fruit'))
+
+# split a string by delimiter
+str_split(my_fruit, pattern = ' ') # bummer: output a list
+# it has to be a list, no one knows how many pieces there will be
+
+# get a character matrix, commit to the number of pieces
+str_split_fixed(my_fruit, pattern = ' ', n = 2)
+
+library(tidyr)
+# if the to-be-split variable lives in a data frame, separate will split it into 2 or more variables
+tibble(my_fruit) %>%
+  separate(my_fruit, into = c('pre', 'post'), sep = ' ')
+
+# string extraction
+length(my_fruit) # the number of strings in a vector
+str_length(my_fruit) # the number of characters in each string
+
+head(fruit) %>%
+  str_sub(1, 3) # start = 1, end = 3, applies to all elements
+
+tibble(fruit) %>%
+  head() %>%
+  mutate(snip = str_sub(fruit, 1:6, 3:8)) # start = 1:6, end = 3:8
+
+# replacement
+x = head(fruit)
+str_sub(x, 1, 3) <- 'AAA'
+
+# collapse a vector
+head(fruit) %>%
+  str_c(collapse = ', ')
+
+# catenate multiple vectors
+# if vectors have the same length, in other words, the same number of elements
+str_c(fruit[1:4], fruit[5:8], sep = " & ")
+# combined with collapsing
+str_c(fruit[1:4], fruit[5:8], sep = " & ", collapse = ", ")
+
+# tidyr
+fruit_df <- tibble(
+  fruit1 = fruit[1:4],
+  fruit2 = fruit[5:8]
+)
+
+fruit_df %>%
+  unite("flavor_combo", fruit1, fruit2, sep = ' & ')
+
+# replacement with a pattern
+str_replace(my_fruit, pattern = "fruit", replacement = "THINGY")
+
